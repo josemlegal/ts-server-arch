7b2c96db881c1b08d03dcf99224846e4d76fd48d
josemlegal <josemlegal97@gmail.com>
2023-02-14 10:56:31 -0300

    Add User and Tx Models

---
+++ src/feature/user/domain/models/transaction.ts
@@ -0,0 +1,6 @@
+export interface Transaction {
+  id: string;
+  title: string;
+  description: string;
+  monto: number;
+}
---
+++ src/feature/user/domain/models/user.ts
@@ -0,0 +1,8 @@
+//Creacion de la interfaz de usuario agregandole el export.
+
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
