5641c2d5109888804595207d2d63ab278bea7f73
josemlegal <josemlegal97@gmail.com>
2023-03-09 23:24:38 -0300

    Add the transaction router

---
+++ src/core/routing/index.ts
@@ -0,0 +1,7 @@
+// import express from 'express';
+
+// export const configureRoutes = (app: express.Application) => {
+//     const router = express.Router();
+//     app.use('/api', router);
+//     router.user('/transaction', userRouter);
+// }
