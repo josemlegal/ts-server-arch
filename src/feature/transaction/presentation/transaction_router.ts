a9c8736f8e89764953483a4e643cc700f896d1a1
josemlegal <josemlegal97@gmail.com>
2023-03-11 15:12:02 -0300

    Add the get tx and update tx endpoints in the presentation layer

--- src/feature/transaction/presentation/transaction_router.ts
+++ src/feature/transaction/presentation/transaction_router.ts
@@ -9,7 +9,7 @@ export default function transactionsRouter(
   router.post("/", async (req, res) => {
     try {
       const newTx = await transactionRepository.createTx(req.body);
-      res.status(201).send(newTx);
+      res.status(200).send(newTx);
     } catch (err) {
       res.status(500).json({ message: "Could not create a transaction" });
     }
@@ -20,7 +20,27 @@ export default function transactionsRouter(
       const allTx = await transactionRepository.getAllTx();
       res.status(200).send(allTx);
     } catch (err) {
-      res.status(500).json({ message: "Could not get transactions" });
+      res.status(500).json({ message: "Could not get all transactions" });
+    }
+  });
+
+  router.get("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const tx = await transactionRepository.getSingleTx(id);
+      res.status(200).send(tx);
+    } catch (err) {
+      res.status(500).json({ message: "Could not get the single transaction" });
+    }
+  });
+
+  router.patch("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const tx = await transactionRepository.updateTx(id, req.body);
+      res.status(200).send(tx);
+    } catch (error) {
+      res.status(500).json({ message: "Could not update the transaction" });
     }
   });
 
