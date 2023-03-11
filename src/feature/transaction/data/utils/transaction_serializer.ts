624769e96a9c00ff52beda42eb5abf7dfd772f77
josemlegal <josemlegal97@gmail.com>
2023-03-11 13:54:50 -0300

    Change the transaction serializer

--- src/feature/transaction/data/utils/transaction_serializer.ts
+++ src/feature/transaction/data/utils/transaction_serializer.ts
@@ -2,9 +2,9 @@ import { Transaction } from "../../domain/models/transaction";
 
 export const transactionFromPG = (item: any): Transaction => {
   return {
-    id: item.transaction.id,
-    title: item.transaction.title,
-    description: item.transaction.description,
-    amount: item.transaction.amount,
+    id: item.id,
+    title: item.title,
+    description: item.description,
+    amount: item.amount,
   };
 };
