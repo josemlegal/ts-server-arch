d5b367d9540d2d505ca80360c951e695d74bad0d
josemlegal <josemlegal97@gmail.com>
2023-03-11 15:10:07 -0300

    Add the update tx query

--- src/feature/transaction/data/query_scripts/queries.ts
+++ src/feature/transaction/data/query_scripts/queries.ts
@@ -1,7 +1,15 @@
 export const SELECT_TRANSACTIONS_QUERY = `SELECT * FROM public.transactions;`;
 
+export const SELECT_TRANSACTION_QUERY = `SELECT * FROM public.transactions WHERE id = $1;`;
+
 export const INSERT_TRANSACTION_QUERY = `
 INSERT INTO public.transactions (title, description, amount)
 VALUES ($1, $2, $3)
 RETURNING *;
+`;
+
+export const UPDATE_TRANSACTION_QUERY = `
+UPDATE public.transactions
+SET title=$2, description=$3, amount=$4
+WHERE id=$1 RETURNING *;
 `;
