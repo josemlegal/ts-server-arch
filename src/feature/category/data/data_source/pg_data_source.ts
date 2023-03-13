b2fcd04107676ba07d86b6b3d9f1034c5f0956e4
josemlegal <josemlegal97@gmail.com>
2023-03-13 00:20:10 -0300

    Add the category data source in the data layer

---
+++ src/feature/category/data/data_source/pg_data_source.ts
@@ -0,0 +1,82 @@
+import { Pool, PoolClient, QueryResult } from "pg";
+import {
+  Category,
+  CreateCategory,
+  UpdateCategory,
+} from "../../domain/models/category";
+import { CategoryDataSource } from "../interfaces/category_data_source";
+import {
+  DELETE_CATEGORY_QUERY,
+  INSERT_CATEGORY_QUERY,
+  SELECT_CATEGORIES_QUERY,
+  SELECT_CATEGORY_QUERY,
+  UPDATE_CATEGORY_QUERY,
+} from "../query_scripts/queries";
+import { categoryFromPG } from "../utils/category_serializer";
+
+export class PGCategoryDataSource implements CategoryDataSource {
+  private db: Pool;
+  private constructor(db: Pool) {
+    this.db = db;
+  }
+  static create(dataSource: Pool) {
+    if (PGCategoryDataSource.instance == null) {
+      PGCategoryDataSource.instance = new PGCategoryDataSource(dataSource);
+    }
+    return PGCategoryDataSource.instance;
+  }
+
+  async getCategory(id: string): Promise<Category> {
+    return await this.callDatabase(SELECT_CATEGORY_QUERY, [id], (result) =>
+      categoryFromPG(result.rows[0])
+    );
+  }
+  async getAllCategories(): Promise<Category[]> {
+    return await this.callDatabase(SELECT_CATEGORIES_QUERY, [], (result) =>
+      result.rows.map(categoryFromPG)
+    );
+  }
+  async createCategory(data: CreateCategory): Promise<Category> {
+    return await this.callDatabase(
+      INSERT_CATEGORY_QUERY,
+      [data.title],
+      (result) => result.rows[0]
+    );
+  }
+  async updateCategory(data: UpdateCategory): Promise<Category> {
+    return await this.callDatabase(
+      UPDATE_CATEGORY_QUERY,
+      [data.id, data.title],
+      (result) => result.rows[0]
+    );
+  }
+  async deleteCategory(id: string): Promise<Category> {
+    return await this.callDatabase(
+      DELETE_CATEGORY_QUERY,
+      [id],
+      (result) => result.rows[0]
+    );
+  }
+
+  static instance: PGCategoryDataSource | null = null;
+
+  private async callDatabase<T>(
+    query: string,
+    values: any[],
+    callback: (result: QueryResult<any>) => T
+  ): Promise<T> {
+    let client: PoolClient;
+    client = await this.db.connect();
+
+    try {
+      const response = await client.query(query, values);
+      return callback(response);
+    } catch (error) {
+      throw error;
+    } finally {
+      if (client) {
+        client.release();
+      }
+    }
+  }
+}
