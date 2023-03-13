73bd07af7cff92272e8a25534111d184275d5929
josemlegal <josemlegal97@gmail.com>
2023-03-13 00:17:49 -0300

    Add the /category in the routing

--- src/core/routing/index.ts
+++ src/core/routing/index.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import categoriesRouter from "../../feature/category/presentation/category_router";
 import { transactionsRouter } from "../../feature/transaction/presentation";
 import usersRouter from "../../feature/user/presentation/user_router";
 
@@ -6,5 +7,6 @@ export const configureRoutes = (app: Application) => {
   const router = express.Router();
   router.use("/transaction", transactionsRouter);
   router.use("/user", usersRouter);
+  router.use("category", categoriesRouter);
   app.use("/api", router);
 };
