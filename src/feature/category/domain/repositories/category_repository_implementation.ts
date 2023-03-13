3afddf936f75a31a7c6dde3686a7130894fce914
josemlegal <josemlegal97@gmail.com>
2023-03-13 00:18:59 -0300

    Add the Category Repository Implementation in the Domain Layer

---
+++ src/feature/category/domain/repositories/category_repository_implementation.ts
@@ -0,0 +1,61 @@
+import { CategoryDataSource } from "../../data/interfaces/category_data_source";
+import { Category, CreateCategory, UpdateCategory } from "../models/category";
+import { CategoryRepository } from "./category_repository";
+
+export class CategoryRepositoryImplementation implements CategoryRepository {
+  private categoryDataSource: CategoryDataSource;
+  static CategoryRepositoryImplementation: CategoryRepositoryImplementation;
+  private constructor(dataSource: CategoryDataSource) {
+    this.categoryDataSource = dataSource;
+  }
+  async getCategory(id: string): Promise<Category> {
+    return await this.callDataSource(() =>
+      this.categoryDataSource.getCategory(id)
+    );
+  }
+  async getAllCategories(): Promise<Category[]> {
+    return await this.callDataSource(() =>
+      this.categoryDataSource.getAllCategories()
+    );
+  }
+  async createCategory(data: CreateCategory): Promise<Category> {
+    return await this.callDataSource(() =>
+      this.categoryDataSource.createCategory(data)
+    );
+  }
+  async updateCategory(id: string, data: UpdateCategory): Promise<Category> {
+    return await this.callDataSource(async () => {
+      const selectedCategory = await this.categoryDataSource.getCategory(id);
+      const updatedCategory = {
+        ...selectedCategory,
+        ...data,
+      };
+      this.categoryDataSource.updateCategory(updatedCategory);
+    });
+  }
+  async deleteCategory(id: string): Promise<Category> {
+    return await this.callDataSource(() =>
+      this.categoryDataSource.deleteCategory(id)
+    );
+  }
+
+  static instance: CategoryRepository | null = null;
+
+  static create(dataSource: CategoryDataSource) {
+    if (CategoryRepositoryImplementation.instance == null) {
+      CategoryRepositoryImplementation.instance =
+        new CategoryRepositoryImplementation(dataSource);
+    }
+    return CategoryRepositoryImplementation.instance;
+  }
+
+  private async callDataSource<T>(callback: Function): Promise<T> {
+    try {
+      return await callback();
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+    // throw new GenericError("Users Repositories error.");
+  }
+}
