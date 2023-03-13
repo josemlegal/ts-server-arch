5ac4e2ed34f328334ae55335dbdb02ba5a257657
josemlegal <josemlegal97@gmail.com>
2023-03-12 22:04:55 -0300

    Add the /transaction route in the routing

--- src/core/routing/index.ts
+++ src/core/routing/index.ts
@@ -1,8 +1,10 @@
 import express, { Application } from "express";
 import { transactionsRouter } from "../../feature/transaction/presentation";
+import usersRouter from "../../feature/user/presentation/user_router";
 
 export const configureRoutes = (app: Application) => {
   const router = express.Router();
   router.use("/transaction", transactionsRouter);
+  router.use("/user", usersRouter);
   app.use("/api", router);
 };
