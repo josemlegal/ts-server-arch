dedaf752144e029acbb01fab8e5e7ac277b4bfc1
josemlegal <josemlegal97@gmail.com>
2023-03-14 11:20:45 -0300

    Add /payment-method in the routing

--- src/core/routing/index.ts
+++ src/core/routing/index.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import categoriesRouter from "../../feature/category/presentation/category_router";
+import paymentMethodsRouter from "../../feature/payment_method/presentation/payment_method_router";
 import { transactionsRouter } from "../../feature/transaction/presentation";
 import usersRouter from "../../feature/user/presentation/user_router";
 
@@ -7,6 +8,7 @@ export const configureRoutes = (app: Application) => {
   const router = express.Router();
   router.use("/transaction", transactionsRouter);
   router.use("/user", usersRouter);
-  router.use("category", categoriesRouter);
+  router.use("/category", categoriesRouter);
+  router.use("/payment-method", paymentMethodsRouter);
   app.use("/api", router);
 };
