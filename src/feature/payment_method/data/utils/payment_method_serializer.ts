8528e4f6519b6df1eb0fe0ca2b6b938565700856
josemlegal <josemlegal97@gmail.com>
2023-03-14 11:22:17 -0300

    Add the PaymentMethod serializer

---
+++ src/feature/payment_method/data/utils/payment_method_serializer.ts
@@ -0,0 +1,8 @@
+import { PaymentMethod } from "../../domain/models/payment_method";
+
+export const paymentMethodFromPG = (item: any): PaymentMethod => {
+  return {
+    id: item.id,
+    title: item.title,
+  };
+};
