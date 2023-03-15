c20840e351d6ca2c08e0cd5661bd5425bb98617b
josemlegal <josemlegal97@gmail.com>
2023-03-15 10:58:51 -0300

    Add the transaction type queries in the domain layer

---
+++ src/feature/transaction_type/data/query_scripts/queries.ts
@@ -0,0 +1,20 @@
+export const SELECT_TRANSACTION_TYPES_QUERY = `SELECT * FROM public.transaction_type;`;
+
+export const SELECT_TRANSACTION_TYPE_QUERY = `SELECT * FROM public.transaction_type WHERE id = $1;`;
+
+export const INSERT_TRANSACTION_TYPE_QUERY = `
+INSERT INTO public.transaction_type (title)
+VALUES ($1)
+RETURNING *;
+`;
+
+export const UPDATE_TRANSACTION_TYPE_QUERY = `
+UPDATE public.transaction_type
+SET title=$2
+WHERE id=$1 RETURNING *;
+`;
+
+export const DELETE_TRANSACTION_TYPE_QUERY = `
+DELETE from public.transaction_type
+WHERE id=$1 RETURNING *;
+`;
