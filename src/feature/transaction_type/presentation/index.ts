a4cc3c407a6701f0ea9821a75a172a3df7be6c3b
josemlegal <josemlegal97@gmail.com>
2023-03-18 23:21:22 -0300

    Implement pools in the transactionTypesRouter

---
+++ src/feature/transaction_type/presentation/index.ts
@@ -0,0 +1,13 @@
+import { pool } from "../../../core/services/database_service";
+import { PGTransactionTypeDataSource } from "../data/data_source/pg_data_source";
+import { TransactionTypeRepositoryImplementation } from "../domain/repositories/transaction_type_implementation";
+import TransactionTypeRouter from "./transaction_type_router";
+
+const transactionTypesDataSource = PGTransactionTypeDataSource.create(pool);
+
+const transactionsTypeRepository =
+  TransactionTypeRepositoryImplementation.create(transactionTypesDataSource);
+
+export const transactionTypesRouter = TransactionTypeRouter(
+  transactionsTypeRepository
+);
