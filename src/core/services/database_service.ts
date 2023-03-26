6e7fdf664853e66ba6fae144da3d18e3672cbad2
josemlegal <josemlegal97@gmail.com>
2023-03-25 22:54:58 -0300

    Update the database conection url

--- src/core/services/database_service.ts
+++ src/core/services/database_service.ts
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  connectionString: "postgres://postgres:postgres@localhost:5432/budget-app",
+  connectionString: "postgres://postgres:postgres@localhost:5432/budget",
 });
