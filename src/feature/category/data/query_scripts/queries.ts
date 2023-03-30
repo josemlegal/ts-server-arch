d14753a332684b3bc8080c9415635073fd779cb3
josemlegal <josemlegal97@gmail.com>
2023-03-30 10:36:36 -0400

    Update the Category Queries

--- src/feature/category/data/query_scripts/queries.ts
+++ src/feature/category/data/query_scripts/queries.ts
@@ -1,16 +1,16 @@
-export const SELECT_CATEGORY_QUERY = `SELECT * FROM public.category;`;
+export const SELECT_CATEGORIES_QUERY = `SELECT * FROM public.category;`;
 
-export const SELECT_CATEGORIES_QUERY = `SELECT * FROM public.category WHERE id = $1;`;
+export const SELECT_CATEGORY_QUERY = `SELECT * FROM public.category WHERE id = $1;`;
 
 export const INSERT_CATEGORY_QUERY = `
-INSERT INTO public.category (title, description, amount)
-VALUES ($1, $2, $3)
+INSERT INTO public.category (title, transaction_type_id)
+VALUES ($1, $2)
 RETURNING *;
 `;
 
 export const UPDATE_CATEGORY_QUERY = `
 UPDATE public.category
-SET title=$2, description=$3, amount=$4
+SET title=$2, transaction_type_id=$3
 WHERE id=$1 RETURNING *;
 `;
 
