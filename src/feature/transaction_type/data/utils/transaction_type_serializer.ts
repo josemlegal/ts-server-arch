fa3d182d7cce67de28a5123d032e05fecb73e9ee
josemlegal <josemlegal97@gmail.com>
2023-03-30 10:38:10 -0400

    Update the Tx Type Serializer

--- src/feature/transaction_type/data/utils/transaction_type_serializer.ts
+++ src/feature/transaction_type/data/utils/transaction_type_serializer.ts
@@ -3,6 +3,6 @@ import { TransactionType } from "../../domain/models/transaction_type";
 export const transactionTypeFromPG = (item: any): TransactionType => {
   return {
     id: item.id,
-    title: item.title,
+    type: item.type,
   };
 };
