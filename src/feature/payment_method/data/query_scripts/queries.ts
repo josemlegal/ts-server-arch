e8fafa23a0719d0def5a1ee40d91f07055a0c4d3
josemlegal <josemlegal97@gmail.com>
2023-03-30 10:37:18 -0400

    Update the Payment Method Queries

--- src/feature/payment_method/data/query_scripts/queries.ts
+++ src/feature/payment_method/data/query_scripts/queries.ts
@@ -1,20 +1,20 @@
-export const SELECT_PAYMENT_METHODS_QUERY = `SELECT * FROM public.payment-method;`;
+export const SELECT_PAYMENT_METHODS_QUERY = `SELECT * FROM public.payment_method;`;
 
-export const SELECT_PAYMENT_METHOD_QUERY = `SELECT * FROM public.payment-method WHERE id = $1;`;
+export const SELECT_PAYMENT_METHOD_QUERY = `SELECT * FROM public.payment_method WHERE id = $1;`;
 
 export const INSERT_PAYMENT_METHOD_QUERY = `
-INSERT INTO public.payment-method (title)
-VALUES ($1)
+INSERT INTO public.payment_method (title, user_id)
+VALUES ($1, $2)
 RETURNING *;
 `;
 
 export const UPDATE_PAYMENT_METHOD_QUERY = `
-UPDATE public.payment-method
-SET title=$2, 
+UPDATE public.payment_method
+SET title=$2, user_id=$3
 WHERE id=$1 RETURNING *;
 `;
 
 export const DELETE_PAYMENT_METHOD_QUERY = `
-DELETE from public.payment-method
+DELETE from public.payment_method
 WHERE id=$1 RETURNING *;
 `;
