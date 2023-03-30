fb74c84df86809fd54dcdacba49277f21cf44a69
josemlegal <josemlegal97@gmail.com>
2023-03-30 10:37:57 -0400

    Update the Tx Type Queries

--- src/feature/transaction_type/data/query_scripts/queries.ts
+++ src/feature/transaction_type/data/query_scripts/queries.ts
@@ -3,14 +3,14 @@ export const SELECT_TRANSACTION_TYPES_QUERY = `SELECT * FROM public.transaction_
 export const SELECT_TRANSACTION_TYPE_QUERY = `SELECT * FROM public.transaction_type WHERE id = $1;`;
 
 export const INSERT_TRANSACTION_TYPE_QUERY = `
-INSERT INTO public.transaction_type (title)
+INSERT INTO public.transaction_type (type)
 VALUES ($1)
 RETURNING *;
 `;
 
 export const UPDATE_TRANSACTION_TYPE_QUERY = `
 UPDATE public.transaction_type
-SET title=$2
+SET type=$2
 WHERE id=$1 RETURNING *;
 `;
 
