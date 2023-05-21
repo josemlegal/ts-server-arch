cfc8d932c657d4bed4e09ee96cb42a71ebdd4861
josemlegal <josemlegal97@gmail.com>
2023-05-20 23:22:06 -0400

    Update conection string

--- src/core/services/postgres/database_config.ts
+++ src/core/services/postgres/database_config.ts
@@ -1,9 +1,8 @@
+import "dotenv/config";
+
 export const config = {
-  env: process.env.NODE_ENV || "dev",
-  port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
+  name: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  port: process.env.DB_PORT || 3000,
+  dbname: process.env.DB_NAME || "postgres",
 };
--- src/core/services/postgres/database_service_pool.ts
+++ src/core/services/postgres/database_service_pool.ts
@@ -2,5 +2,5 @@ import { Pool } from "pg";
 import { config } from "./database_config";
 
 export const pool = new Pool({
-  connectionString: `postgres://${config.dbUser}:${config.dbPassword}@${config.dbPort}:${config.dbPort}/${config.dbName}`,
+  connectionString: `postgres://${config.name}:${config.password}@localhost:${config.port}/${config.dbname}`,
 });
