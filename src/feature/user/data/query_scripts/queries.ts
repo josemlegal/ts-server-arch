c557b28d7dfb60cb0e54dfc06df151014e006c82
josemlegal <josemlegal97@gmail.com>
2023-05-20 22:42:47 -0400

    Update user queries and serializer

--- src/feature/user/data/query_scripts/queries.ts
+++ src/feature/user/data/query_scripts/queries.ts
@@ -3,14 +3,14 @@ export const SELECT_USERS_QUERY = `SELECT * FROM public.user;`;
 export const SELECT_USER_QUERY = `SELECT * FROM public.user WHERE id = $1;`;
 
 export const INSERT_USER_QUERY = `
-INSERT INTO public.user (name, email, password)
-VALUES ($1, $2, $3)
+INSERT INTO public.user (first_name, last_name, email, password)
+VALUES ($1, $2, $3, $4)
 RETURNING *;
 `;
 
 export const UPDATE_USER_QUERY = `
 UPDATE public.user
-SET name=$2, email=$3, password=$4
+SET first_name=$2, last_name=$3, email=$4, password=$5
 WHERE id=$1 RETURNING *;
 `;
 
--- src/feature/user/data/utils/user_serializer.ts
+++ src/feature/user/data/utils/user_serializer.ts
@@ -3,7 +3,8 @@ import { User } from "../../domain/models/user";
 export const userFromPG = (item: any): User => {
   return {
     id: item.id,
-    name: item.name,
+    firstName: item.first_name,
+    lastName: item.last_name,
     email: item.email,
     password: item.password,
   };
